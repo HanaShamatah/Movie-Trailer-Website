fc003694bc3e3f86d787a30d658ff74b4ea9ef04
HanaShamatah <37618147+HanaShamatah@users.noreply.github.com>
2018-06-01 17:32:00 +0100

    Update media.py

--- media.py
+++ media.py
@@ -10,7 +10,7 @@ class Movie():
         self.storyline = movie_storyline
         self.poster_image_url = movie_image_url
         self.trailer_youtube_url = movie_trailer_url
-
+    # define two functions to open trailer and poster image
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
 
