53e5c5ff50ccc8c7681d0171deff3ae051f65f90
HanaShamatah <37618147+HanaShamatah@users.noreply.github.com>
2018-06-01 02:01:25 +0100

    Update media.py

--- media.py
+++ media.py
@@ -4,14 +4,15 @@ import webbrowser
 
 class Movie():
     "Thia class is for saving movies info"
-    def __init__(self, movie_title, movie_storyline, movie_image_url, movie_trailer_url):
+    def __init__(self, movie_title, movie_storyline,
+                 movie_image_url, movie_trailer_url):
         self.title = movie_title  # from 12 to 15 line are instance variables
         self.storyline = movie_storyline
         self.poster_image_url = movie_image_url
         self.trailer_youtube_url = movie_trailer_url
 
-    def show_trailer(self):  # to open trailer url
+    def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
 
-    def show_poster(self):  # to open poster url
+    def show_poster(self):
         webbrowser.open(self.poster_image_url)
